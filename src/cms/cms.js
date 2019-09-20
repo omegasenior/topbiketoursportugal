86fa4fe4ce4bda67c4e1e75c445cce346ee0e634
Nuno Costa <nuno.costa@devscope.net>
2019-09-20 11:48:43 +0100

    Preview pages tour

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,8 @@
 import CMS from "netlify-cms";
 
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import TourGenPreview from "./preview-templates/TourGenPreview";
+import TourPagePreview from "./preview-templates/TourPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import ProductPagePreview from "./preview-templates/ProductPagePreview";
  import NetlifyCmsWidgetUUID from "./widgets/netlifly-cms-widget-uuid";
@@ -9,6 +11,8 @@ import UUIDControl from "./widgets/netlifly-cms-widget-uuid/UUIDControl";
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
+CMS.registerPreviewTemplate("blog", TourPagePreview);
+CMS.registerPreviewTemplate("blog", TourGenPreview);
 
 CMS.registerWidget('uuid',NetlifyCmsWidgetUUID.controlComponent,NetlifyCmsWidgetUUID.previewComponent);
 
