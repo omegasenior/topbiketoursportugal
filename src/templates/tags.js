b424d1d686b4b34c508f94ff3d9cb1fa8f617b5e
Nuno Costa <nuno.costa@devscope.net>
2019-12-22 01:24:51 +0000

    fix tags broken links

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -8,7 +8,7 @@ class TagRoute extends React.Component {
     const posts = this.props.data.allMarkdownRemark.edges
     const postLinks = posts.map(post => (
       <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
+        <Link to={post.node.frontmatter.path || post.node.fields.slug}>
           <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
         </Link>
       </li>
@@ -33,7 +33,7 @@ class TagRoute extends React.Component {
                 <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
                 <ul className="taglist">{postLinks}</ul>
                 <p>
-                  <Link to="/tags/">Browse all tags</Link>
+                  <Link to="/tags">Browse all tags</Link>
                 </p>
               </div>
             </div>
@@ -66,6 +66,7 @@ export const tagPageQuery = graphql`
           }
           frontmatter {
             title
+            path
           }
         }
       }
